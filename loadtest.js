02d801d49e34a5c8407772457deb4d06ffcfd84e
RenatoGoncalvesBR <renato.goncalves86@hotmail.com>
2026-03-27 19:16:48 -0300

    Loadtest com upgrade para 100vu e 60seg

--- loadtest.js
+++ loadtest.js
@@ -2,13 +2,13 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 20, // usuários virtuais
-  duration: '20s', // tempo do teste
+  vus: 100, // usuários virtuais
+  duration: '60s', // tempo do teste
   summaryTimeUnit: 'ms', // padronizar saída em milissegundos
 };
 
 export default function () {
-  const res = http.get('https://jsonplaceholder.typicode.com/posts/999999');
+  const res = http.get('https://jsonplaceholder.typicode.com/posts');
 
   check(res, {
     'status é 200': (r) => r.status === 200,
