78ea618afb378de3d59c0363cb1cdf5f3982922f
RenatoGoncalvesBR <renato.goncalves86@hotmail.com>
2026-03-27 18:12:31 -0300

    Correção site loadtest.js

---
+++ loadtest.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 20, // usuários virtuais
+  duration: '20s', // tempo do teste
+};
+
+export default function () {
+  const res = http.get('https://jsonplaceholder.typicode.com/posts/999999');
+
+  check(res, {
+    'status é 200': (r) => r.status === 200,
+    'tempo resposta < 500ms': (r) => r.timings.duration < 500,
+  });
+
+  sleep(1);
+}
